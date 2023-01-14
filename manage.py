1c565efaac76ff7df85328a3752714e40856ba6d
fabianomobono <fabian.omobono@gmail.com>
2023-01-13 21:10:52 -0700

    permissions

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+<<<<<<< HEAD
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_django_project.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+>>>>>>> b4063b027e7c189885de7f663e09db47f2387fe6
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
